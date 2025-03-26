9ec6dd2c77954ecfa1bab32e287abb0fafb4dde2
FradeAlexandre <alexandre.santos150@fatec.sp.gov.br>
2025-03-26 19:49:32 -0300

    Atualização 26.03

--- src/main/java/com/fatec/projeto/projeto2025/domain/cliente/ClienteService.java
+++ src/main/java/com/fatec/projeto/projeto2025/domain/cliente/ClienteService.java
@@ -1,6 +1,7 @@
 package com.fatec.projeto.projeto2025.domain.cliente;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +16,25 @@ public class ClienteService {
     public List<Cliente> listarClientes(){
         return clienteRepository.findAll();
     }
+
+    public boolean deletarCliente(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletarCliente'");
+    }
+
+    public Optional<Cliente> buscarClientePorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarClientePorId'");
+    }
+
+    public Cliente criarCliente(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'criarCliente'");
+    }
+
+    public boolean atualizarCliente(Long id, Cliente clienteAtualizado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarCliente'");
+    }
 }
+
